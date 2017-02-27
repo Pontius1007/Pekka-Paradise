24c6abba40396168fbede8516177c08687f37f1d
Håvard Hellem <haavahe@stud.ntnu.no>
2017-02-27 16:39:21 +0100

    config

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
-app.config.from_object('config')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 db = SQLAlchemy(app)
