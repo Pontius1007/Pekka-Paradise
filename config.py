aab5bb8d7b6b845275ac77d57a5bd52dbeefaa8b
Håvard Hellem <haavahe@stud.ntnu.no>
2017-02-27 16:36:08 +0100

    test

--- config.py
+++
@@ -1,7 +0,0 @@
-import os
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-
-DEBUG = os.environ.get('DEBUG', 0)
-SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
-SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
