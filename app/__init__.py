d8ce95e6c2edffe581819add8df01d6b9ce1d6a9
Håvard Hellem <haavahe@stud.ntnu.no>
2017-02-28 15:02:06 +0100

    testing new file structure

--- app/__init__.py
+++ app/__init__.py
@@ -6,3 +6,4 @@ app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)
 
+from app import limbo
--- db_create.py
+++ db_create.py
@@ -10,4 +10,4 @@ if not os.path.exists(SQLALCHEMY_MIGRATE_REPO):
     api.create(SQLALCHEMY_MIGRATE_REPO, 'database repository')
     api.version_control(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)
 else:
-    api.version_control(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO, api.version(SQLALCHEMY_MIGRATE_REPO))  # noqa
+    api.version_control(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO, api.version(SQLALCHEMY_MIGRATE_REPO))
