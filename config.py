bca0a02407ba6d13dedf4ce46e8d34a8edf04c87
Håvard Hellem <haavahe@stud.ntnu.no>
2017-02-28 15:34:35 +0100

    supressing warning

--- config.py
+++ config.py
@@ -4,3 +4,4 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+SQLALCHEMY_TRACK_MODIFICATIONS = True
