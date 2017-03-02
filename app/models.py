3e1cfb8cddb0e725642654166efd1012d4ac6278
Håvard Hellem <haavahe@stud.ntnu.no>
2017-03-02 11:30:38 +0100

    Made changes to models: changed user and added course

--- app/models.py
+++ app/models.py
@@ -1,9 +1,23 @@
 from app import db
 
+
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    nickname = db.Column(db.String(64), index=True, unique=True)
-    email = db.Column(db.String(120), index=True, unique=True)
+    user_id = db.Column(db.Integer, primary_key=True)
+    course_id = db.Column(db.String(10), db.foreignKey(''))
+
+    def __init__(self, user_id):
+        self.user_id = user_id
 
     def __repr__(self):
-        return '<User %r>' % (self.nickname)
+        return '<User %r>' % self.user_id
+
+
+class Course(db.Model):
+    course_id = db.Column(db.String(10), primary_key=True)
+    course_name = db.Column(db.String(50), unique=True)
+
+    def __init__(self, course_id):
+        self.course_id = course_id
+
+    def __repr__(self):
+        return '<Course %r>' % self.course_id
