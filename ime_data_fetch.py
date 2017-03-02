bc333bd0b0400f465da4be64308930c1188ae715
Ola Richard Syversen <olars@stud.ntnu.no>
2017-03-02 13:42:52 +0100

    Added various methods for retreiving information about subjects

--- ime_data_fetch.py
+++ ime_data_fetch.py
@@ -3,9 +3,9 @@ import requests
 
 
 base_url = "http://www.ime.ntnu.no/api/course/en/"
+
+
 # method for checking if a subject exists at NTNU
-
-
 def subject_exists(code):
 
     try:
@@ -17,3 +17,15 @@ def subject_exists(code):
     except ValueError:
         return 'Not valid'
     return ''.join(code + ': ' + name)
+
+
+# same as above but returns a boolean value instead
+def subject_exists_boolean(code):
+
+    try:
+        requests.get(base_url + code).json()
+    except TypeError:
+        return False
+    except ValueError:
+        return False
+    return True
