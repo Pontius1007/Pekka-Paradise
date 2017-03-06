c1903d2dd4812d14b7c258def0f8fcb6d6758d3d
Pontius1007 <embrikeinang@msn.com>
2017-03-06 17:41:39 +0100

    Changed the test URL

--- app/limbo.py
+++ app/limbo.py
@@ -72,7 +72,7 @@ def send_message(token, recipient, text):
 
 def test_get_full_name(sender):
     print(sender)
-    test_url = "https://graph.facebook.com/v2.6/" + sender + "fields=first_name,last_name&access_token=" + PAT
+    test_url = "https://graph.facebook.com/v2.6/" + sender + "?fields=first_name,last_name&access_token=" + PAT
     print(test_url)
     test = requests.get(test_url)
     print(test)
