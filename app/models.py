c827e013d00ed099f9105df666fd62413b5976d8
Håvard Hellem <haavahe@stud.ntnu.no>
2017-03-06 16:28:33 +0100

    new models version with migration

--- app/models.py
+++ app/models.py
@@ -1,25 +1,13 @@
 from app import db
 
 
-class User(db.Model):
-    __tablename__ = 'userInformation'
-    user_id = db.Column(db.Integer, primary_key=True)
-    course_id = db.Column(db.String(15), db.ForeignKey('courseInformation.course_id'))
+class UserFacebook(db.Model):
+    __tablename__ = 'userfacebook'
+    user_id = db.Column(db.String(100), primary_key=True)
+    subject = db.Column(db.String(20))
 
     def __init__(self, user_id):
         self.user_id = user_id
 
     def __repr__(self):
         return '<User %r>' % self.user_id
-
-
-class Course(db.Model):
-    __tablename__ = 'courseInformation'
-    course_id = db.Column(db.String(15), primary_key=True)
-    course_name = db.Column(db.String(50), unique=True)
-
-    def __init__(self, course_id):
-        self.course_id = course_id
-
-    def __repr__(self):
-        return '<Course %r>' % self.course_id
