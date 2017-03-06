8b523c8b81899d239934fa8f4f04688af87ef3ec
Pontius1007 <embrikeinang@msn.com>
2017-03-06 17:47:46 +0100

    Printing json data

--- app/limbo.py
+++ app/limbo.py
@@ -71,10 +71,9 @@ def send_message(token, recipient, text):
 
 
 def test_get_full_name(sender):
-    print(sender)
     test_url = "https://graph.facebook.com/v2.6/" + sender + "?fields=first_name,last_name&access_token=" + PAT
-    print(test_url)
-    test = requests.get(test_url)
-    print(test)
+    full_name_get = requests.get(test_url)
+    data = json.loads(full_name_get)
+    print(data)
 
 
