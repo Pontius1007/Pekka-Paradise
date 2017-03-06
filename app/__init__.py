c6d677108c97d23f0a2dbd99063a34255ff1959b
Håvard Hellem <haavahe@stud.ntnu.no>
2017-03-06 14:58:31 +0100

    Added models to the bottom import

--- app/__init__.py
+++ app/__init__.py
@@ -1,19 +1,10 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-import os
 
 app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)
 
-# The following could possibly be removed
-if os.environ.get('HEROKU') is not None:
-    import logging
-    stream_handler = logging.StreamHandler()
-    app.logger.addHandler(stream_handler)
-    app.logger.setLevel(logging.INFO)
-    app.logger.info('pekkp startup')
-
 # Import at the end to prevent circular import error with limbo.py
-from app import limbo
+from app import limbo, models
 
