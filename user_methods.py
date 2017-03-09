dc0729fdd053d8d8345244e6d96b3e13dfb9453d
Håvard Hellem <haavahe@stud.ntnu.no>
2017-03-09 10:53:28 +0100

    added method for getting subject from user

--- user_methods.py
+++ user_methods.py
@@ -2,6 +2,11 @@ from app import db, models
 
 
 def has_user(user_name):
+    """
+    check if user in userfacebook has subject
+    :param user_name:
+    :return:
+    """
     try:
         if models.UserFacebook.query.get(user_name) is not None:
             return True
@@ -11,6 +16,11 @@ def has_user(user_name):
 
 
 def add_user(user_name, subject_name):
+    """
+    add a user and subject to userfacebook
+    :param user_name:
+    :param subject_name:
+    """
     try:
         if not has_user(user_name):
             new_user = models.UserFacebook(user_name, subject_name)
@@ -23,6 +33,11 @@ def add_user(user_name, subject_name):
 
 
 def add_subject(user_name, subject_name):
+    """
+    add subject to userfacebook
+    :param user_name:
+    :param subject_name:
+    """
     try:
         if models.UserFacebook.query.get(user_name) is None:
             add_user(user_name, subject_name)
@@ -35,4 +50,16 @@ def add_subject(user_name, subject_name):
         print(e)
 
 
-add_user('Anders Larsen', 'TDT4110')
+def get_subject(user_name):
+    """
+    get subject from userfacebook
+    :param user_name:
+    :return: subject_name
+    """
+    try:
+        if models.UserFacebook.query.get(user_name) is not None:
+            return models.UserFacebook.query.get(user_name).subject
+        else:
+            return None
+    except Exception as e:
+        print(e)
