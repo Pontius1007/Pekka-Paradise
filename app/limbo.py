00f0e500b5b8bc1477479b54b42efe9d3baa21dd
andrefei <andreas.eikeland@hotmail.com>
2017-03-13 15:59:40 +0100

    changed ! to = line 84 limbo

--- app/limbo.py
+++ app/limbo.py
@@ -81,7 +81,7 @@ def handle_messages():
                     user_methods.add_user(user_name, incoming_message.split()[0])
                 response_handler.has_course(PAT, sender, user_methods.get_subject(user_name))
 
-            elif incoming_message != payload:
+            elif incoming_message.lower == payload:
                 response_handler.text_message(PAT, sender, "Type 'help' to see what you can do with L.I.M.B.O.")
 
     return "ok"
