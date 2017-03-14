9b415920575dc204c6eb797bd43dc7cd01be1edd
Håvard Hellem <haavahe@stud.ntnu.no>
2017-03-14 17:17:22 +0100

    Finished methods for adding feedback for a lecture

--- app/limbo.py
+++ app/limbo.py
@@ -82,6 +82,8 @@ def handle_messages():
                     response_handler.lec_feed(PAT, sender)
 
             elif payload == "fast" or payload == "ok" or payload == "slow":
+                # Adds feedback if the subject has a lecture on the given day
+                # and if the user has not already given feedback
                 if feedback_methods.add_entry(user_name, user_methods.get_subject_from_user(user_name), payload):
                     response_handler.text_message(PAT, sender, "You chose: " + payload + "\n Feedback Received!")
                     response_handler.has_course(PAT, sender, user_methods.get_subject_from_user(user_name))
