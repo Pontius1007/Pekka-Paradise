b3beba829c9f5ebd672ba42c60c4d17597a59d53
andrefei <andreas.eikeland@hotmail.com>
2017-03-16 12:28:14 +0100

    Changed url in test method again

--- app/responses.py
+++ app/responses.py
@@ -56,7 +56,7 @@ def test_graph(token, recipient):
                                 "attachment": {
                                   "type": "image",
                                   "payload": {
-                                      "url": "app/lordi_lead.jpg"
+                                      "url": "@lordi_lead.jpg"
                                   }
                                 }
                               }
