4bd42e738f7b3be41179d37497e3d52d01b0c080
Pontius1007 <embrikeinang@msn.com>
2017-03-16 13:55:08 +0100

    Added three methods to get information from the db

--- lecture_feedback_db_methods.py
+++ lecture_feedback_db_methods.py
@@ -1,14 +1,62 @@
 from app import db, models
 
-def get_year(course):
-    try:
-        if models.Lecture.query.filter_by(subject=course):
-            
-feedbacks = []
-lectures = models.Lecture.query.filter_by(subject='TDT4112', year=2017, week_number=11, day_number=2)
-for lecture in lectures:
-    feedbacks.append(models.LectureFeedback.query.filter_by(lecture_id=lecture.id, user_id='Håvard Hellem'))
-for feedback in feedbacks:
-    for feed in feedback:
-        print(feed)
 
+def get_year(course):
+    """
+    :param course:
+    :return A list in descending order containing the years of the lectures:
+    """
+    try:
+        if models.Lecture.query.filter_by(subject=course) is not None:
+            lecture_year = []
+            lectures = models.Lecture.query.filter_by(subject=course)
+            for lecture in lectures:
+                if lecture.year not in lecture_year:
+                    lecture_year.append(lecture.year)
+            lecture_year.sort(reverse=True)
+            return lecture_year
+    except Exception as e:
+        print(e)
+
+
+def check_lecture_semester(course, start, end, year):
+    """
+    :param course:
+    :param start:
+    :param end:
+    :param year:
+    :return True or false depending on is the course has lectures in the given weeks:
+    """
+    try:
+        if models.Lecture.query.filter_by(subject=course) is not None:
+            lecture_week = []
+            lectures = models.Lecture.query.filter_by(subject=course, year=year)
+            for lecture in lectures:
+                if lecture.week_number not in lecture_week:
+                    lecture_week.append(lecture.week_number)
+            for i in range(start, end):
+                if i in lecture_week:
+                    return True
+                else:
+                    return False
+    except Exception as e:
+        print(e)
+
+
+def day_of_lecture_in_week(course, week, year):
+    """
+    :param course:
+    :param week:
+    :param year:
+    :return A list containing the week-days of a given lecture in a given week:
+    """
+    try:
+        if models.Lecture.query.filter_by(subject=course) is not None:
+            lecture_days = []
+            lectures = models.Lecture.query.filter_by(subject=course, year=year, week_number=week)
+            for lecture in lectures:
+                if lecture.day_number not in lecture_days:
+                    lecture_days.append(lecture.day_number)
+            return lecture_days
+    except Exception as e:
+        print(e)
