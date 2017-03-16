e9172aa9cab179b5cadd9c40dfe82e01633eec06
Pontius1007 <embrikeinang@msn.com>
2017-03-16 11:41:33 +0100

    Set VERIFY_TOKEN as a config var

--- config.py
+++ config.py
@@ -5,6 +5,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 # SET DATABASE_URL="LINK-URL" to set it in VENV
 PAT = os.environ['PAT']
+VERIFY_TOKEN = os.environ['VERIFY_TOKEN']
 SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 SQLALCHEMY_TRACK_MODIFICATIONS = True
