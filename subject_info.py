40b981fd0d624e49bffe37c89e050f0ceb80603c
Ola Richard Syversen <olasy95@gmail.com>
2017-03-16 13:57:06 +0100

    added unit tests for subject_info, renamed sub_info to subject_info and fixed a bug in gather_lecture_info

--- subject_info.py
+++ subject_info.py
@@ -10,7 +10,6 @@ week = ["mandag", "tirsdag", "onsdag", "torsdag", "fredag"]
 current_year = str(date.today().year)
 
 
-
 # method that return a courses schedule in JSON format if the subject exists
 def get_schedule(sub_code):
     """
@@ -33,26 +32,6 @@ def get_schedule(sub_code):
         return False
 
 
-def print_schedule(schedule):
-    """
-     method that prints a courses schedule to the console from a JSON schedule file
-    :param schedule: a schedule JSON file
-    :return:
-    """
-    if not schedule:
-        print("No schedule available")
-
-    if not schedule:
-        print("No schedule available")
-
-    print("Timeplan for " + schedule['course']['summarized'][0]['courseName'] + ":")
-    for i in range(0, len(schedule['course']['summarized'])):
-        print(schedule['course']['summarized'][i]['description'] + " "
-              + week[schedule['course']['summarized'][i]['dayNum'] - 1] +
-              " fra " + schedule['course']['summarized'][i]['from'] +
-              " til " + schedule['course']['summarized'][i]['to'])
-
-
 def printable_schedule(schedule):
     """
     pretty much the same as print_schedule except that it returns a formatted string instead of printing it
@@ -62,15 +41,13 @@ def printable_schedule(schedule):
     if not schedule:
         return "No schedule available"
 
-    if not schedule:
-        return "No schedule available"
-
-    schedule_string = "Timeplan for " + schedule['course']['summarized'][0]['courseName'] + ":\n"
-    for i in range(0, len(schedule['course']['summarized'])):
-        schedule_string += (schedule['course']['summarized'][i]['description'] + " "
-                            + week[schedule['course']['summarized'][i]['dayNum'] - 1] +
-                            " fra " + schedule['course']['summarized'][i]['from'] +
-                            " til " + schedule['course']['summarized'][i]['to'] + "\n")
+    schedule = schedule['course']['summarized']
+    schedule_string = "Timeplan for " + schedule[0]['courseName'] + ":\n"
+    for i in range(0, len(schedule)):
+        schedule_string += (schedule[i]['description'] + " "
+                            + week[schedule[i]['dayNum'] - 1] +
+                            " fra " + schedule[i]['from'] +
+                            " til " + schedule[i]['to'] + "\n")
     return schedule_string
 
 
@@ -91,8 +68,11 @@ def gather_lecture_information(schedule):
             single_lecture.extend(
                 (schedule['course']['summarized'][i]['courseCode'], schedule['course']['summarized'][i]['from'],
                  schedule['course']['summarized'][i]['to'], schedule['course']['summarized'][i]['dayNum'],
-                 schedule['course']['summarized'][i]['weeks'], schedule['course']['summarized'][i]['arsterminId'],
-                 schedule['course']['summarized'][i]['rooms'][0]['romNavn']))
+                 schedule['course']['summarized'][i]['weeks'], schedule['course']['summarized'][i]['arsterminId']))
+            try:
+                single_lecture.extend(schedule['course']['summarized'][i]['rooms'][0]['romNavn'])
+            except IndexError:
+                single_lecture.extend("")
             lecture_information.append(single_lecture)
 
     return lecture_information
@@ -143,3 +123,4 @@ def course_name(code):
     c = get_course_json(code)
     return c['course']['name']
 
+print(gather_lecture_information(get_schedule("exph0004")))
--- testing.py
+++ testing.py
@@ -2,11 +2,15 @@
 
 import unittest
 import ime_data_fetch
+import subject_info
 
 
 class Testerino(unittest.TestCase):
 
     def test_subject_exists_boolean(self):
+        """
+        This method tests the subject_exists_boolean method in ime_data_fetch.py
+        """
         self.assertTrue(ime_data_fetch.subject_exists_boolean("tdt4145"))
         self.assertTrue(ime_data_fetch.subject_exists_boolean("tdt4140"))
         self.assertTrue(ime_data_fetch.subject_exists_boolean("afr1000"))
@@ -16,11 +20,62 @@ class Testerino(unittest.TestCase):
         self.assertFalse(ime_data_fetch.subject_exists_boolean(""))
 
     def test_get_subject_name(self):
+        """
+        This method tests the get_subject_name method in ime_data_fetch.py
+        """
         self.assertEqual(ime_data_fetch.get_subject_name("tdt4145"), "TDT4145: Data Modelling, Databases "
                                                                      "and Database Management Systems")
         self.assertEqual(ime_data_fetch.get_subject_name("afr1000"), "AFR1000: Introduction to African Studies")
         self.assertEqual(ime_data_fetch.get_subject_name("tdt1234"), 'Subject does not exist')
         self.assertEqual(ime_data_fetch.get_subject_name(10), 'Subject does not exist')
+
+    def test_get_schedule(self):
+        """
+        This method tests the get_schedule method in subject_info.py
+        """
+        self.assertEqual(subject_info.get_schedule("tdt4145")['course']['summarized'][0]['acronym'], "FOR")
+        self.assertEqual(subject_info.get_schedule("tdt4100")['course']['summarized'][0]['acronym'], "LAB")
+        self.assertFalse(subject_info.get_schedule("tdt123"))
+        self.assertFalse(subject_info.get_schedule(123))
+        self.assertFalse(subject_info.get_schedule("tdt4120"))
+
+    def test_printable_schedule(self):
+        """
+        This method test the printable_schedule method in subject_info.py
+        """
+        week = ["mandag", "tirsdag", "onsdag", "torsdag", "fredag"]
+        test_schedule = subject_info.get_schedule("tdt4145")['course']['summarized']
+        schedule_string = "Timeplan for " + test_schedule[0]['courseName'] + ":\n"
+        for i in range(0, len(test_schedule)):
+            schedule_string += (test_schedule[i]['description'] + " "
+                                + week[test_schedule[i]['dayNum'] - 1] +
+                                " fra " + test_schedule[i]['from'] +
+                                " til " + test_schedule[i]['to'] + "\n")
+        self.assertEqual(subject_info.printable_schedule(subject_info.get_schedule("tdt4145")), schedule_string)
+        self.assertEqual(subject_info.printable_schedule(subject_info.get_schedule("tdt123")), "No schedule available")
+
+    def test_gather_lecture_information(self):
+        """
+        This method tests the gather_lecture_information method in subject_info.py
+        """
+        lecture_information = []
+        schedule = subject_info.get_schedule("exph0004")
+        for i in range(0, len(schedule['course']['summarized'])):
+            if schedule['course']['summarized'][i]['acronym'] == 'FOR':
+                single_lecture = []
+                single_lecture.extend(
+                    (schedule['course']['summarized'][i]['courseCode'], schedule['course']['summarized'][i]['from'],
+                     schedule['course']['summarized'][i]['to'], schedule['course']['summarized'][i]['dayNum'],
+                     schedule['course']['summarized'][i]['weeks'], schedule['course']['summarized'][i]['arsterminId']))
+                try:
+                    single_lecture.extend(schedule['course']['summarized'][i]['rooms'][0]['romNavn'])
+                except IndexError:
+                    single_lecture.extend("")
+                lecture_information.append(single_lecture)
+        self.assertEqual(subject_info.gather_lecture_information(subject_info.get_schedule("exph0004")),
+                         lecture_information)
+        self.assertEqual(subject_info.gather_lecture_information(subject_info.get_schedule("tdt123")),
+                         "No schedule available")
 
 if __name__ == '__main__':
     unittest.main()
