b699415a97101863415a91d2167bdd646b96872f
Håvard Hellem <haavardhellem@gmail.com>
2017-03-20 16:13:10 +0100

    added a test print

--- app/limbo.py
+++ app/limbo.py
@@ -39,6 +39,9 @@ def handle_messages():
     for sender, incoming_message, payload in messaging_events(payload):
         # The following statements check which options the user selected
         # Response handler contains "templates" for the various messages
+        # Start test
+        print(payload)
+        # End test
         user_name = get_full_name(sender, PAT)
         if "hei" in incoming_message.lower() or "hallo" in incoming_message.lower() or "yo" in incoming_message.lower():
             response_handler.greeting_message(PAT, sender, user_name)
