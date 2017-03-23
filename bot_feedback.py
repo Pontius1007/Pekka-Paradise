4ad8618e254eb17fbd89e51ad5a9627f3ed3dd8b
Håvard Hellem <haavardhellem@gmail.com>
2017-03-23 15:10:07 +0100

    debugging all lectures feedback

--- bot_feedback.py
+++ bot_feedback.py
@@ -6,7 +6,6 @@ def generate_percent(feedback):
     :param feedback:
     :return: total number of persons, subject
     """
-    subject = len(feedback[0])
     print(feedback)
     slow = feedback.count(0)
     ok = feedback.count(1)
@@ -14,4 +13,4 @@ def generate_percent(feedback):
     total = float(slow + ok + fast)
     print(total, slow, ok, fast)
     counter = [round((slow/total)*100), round((ok/total)*100), round((fast/total)*100), round(total)]
-    return subject, counter
+    return counter
