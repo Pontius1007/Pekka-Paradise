0685fac116068f95899a316ff32321dbc58d3e76
andrefei <andreas.eikeland@hotmail.com>
2017-03-23 12:03:46 +0100

    Fixed counter to count all feedbacks

--- bot_feedback.py
+++ bot_feedback.py
@@ -6,23 +6,26 @@ def generate_percent(feedback):
     :param feedback:
     :return: total number of persons, subject
     """
-    total_persons = len(feedback) - 1
     subject = len(feedback[0])
-    counter = [0.0, 0.0, 0.0,
-               total_persons]  # First index shows percentage of slow, second ok, third too fast, fourth total number
+    tot = 0
+    counter = [0.0, 0.0, 0.0]  # First index shows percentage of slow, second ok, third too fast, fourth total number
     for i in range(0, len(feedback)):
         if i == 0:
             subject = feedback[i]
         else:
             if feedback[i] == 0:
                 counter[0] += 1
+                tot += 1
             elif feedback[i] == 1:
                 counter[1] += 1
+                tot += 1
             elif feedback[i] == 2:
                 counter[2] += 1
+                tot +=1
 
     counter[0] /= counter[3] * 100
     counter[1] /= counter[3] * 100
     counter[2] /= counter[3] * 100
+    counter.append(tot)
 
     return subject, counter
