fda465b1b1755b97bdc4f76d9f303b34c17856fe
andrefei <andreas.eikeland@hotmail.com>
2017-03-28 10:13:30 +0200

    Captures console output correctly

--- db_tests.py
+++ db_tests.py
@@ -1,21 +1,25 @@
 # -*- coding utf-8 -*-
 import unittest
+import sys
+from io import StringIO
+
 from alt import user_methods
 
 
 class DbTests(unittest.TestCase):
 
-    def test_user_methods(self, capfd):
+    def test_user_methods(self):
         user = "Test User1337"
         subject = "TDT4120"
         user_methods.add_user(user, subject)
         user_methods.add_user(user, subject)
-        out, err = capfd.readouterr()
-        assert out == 'User already exists'
+        captured_output = StringIO()
+        sys.stdout = captured_output
+        sys.stdout = sys.__stdout__
+        self.assertEqual('User already exists', captured_output.getvalue())
         self.assertEqual(user_methods.get_subject_from_user("Test User1337"), "TDT4120")
         user_methods.add_subject("Test User1337", "TMA4100")
         self.assertEqual(user_methods.get_subject_from_user("Test User1337"), "TMA4100")
-        user_methods.add_user("Test User1337", "")
 
         user_methods.delete_user("Test User1337")
 
