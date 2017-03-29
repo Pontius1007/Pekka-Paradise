3092c2cd0ba3f804c513e1248190a292de5be35c
andrefei <andreas.eikeland@hotmail.com>
2017-03-29 13:39:36 +0200

    Removed sql alchemy declaration

--- app/config.py
+++ app/config.py
@@ -70,8 +70,6 @@ class Config(dict):
     """
 
     def __init__(self, root_path, defaults=None):
-        SQLALCHEMY_DATABASE_URI = "postgres://zmfgqtegrrvgfl:4654c37a586e8e982c610e93aa2453618a4970715eef4c10686da44572" \
-                                  "d3d39c@ec2-184-72-216-69.compute-1.amazonaws.com:5432/d8avccro5jjpbi"
         dict.__init__(self, defaults or {})
         self.root_path = root_path
 
