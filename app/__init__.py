5c619d3a728fbfa38b78174edd544fae2493d4ec
andrefei <andreas.eikeland@hotmail.com>
2017-03-29 13:55:13 +0200

    Trting yet another minor change to app/init

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,9 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config.from_pyfile('config.py')
+# app.config.from_pyfile('config.py')
+app.config.from_object('yourapplication.default_settings')
+app.config.from_envvar('YOURAPPLICATION_SETTINGS')
 db = SQLAlchemy(app)
 
 # Import at the end to prevent circular import error with limbo.py
