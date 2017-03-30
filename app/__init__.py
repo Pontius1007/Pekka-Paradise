93bc4a3b2e49fb1fd3dc479c2e4c4c111d71bc00
andrefei <andreas.eikeland@hotmail.com>
2017-03-30 10:22:13 +0200

    Using envvar in app/init

--- app/__init__.py
+++ app/__init__.py
@@ -3,8 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 # app.config.from_pyfile('config.py')
-app.config.from_object('yourapplication.default_settings')
-app.config.from_envvar('YOURAPPLICATION_SETTINGS')
+app.config.from_envvar('../config.py')
 db = SQLAlchemy(app)
 
 # Import at the end to prevent circular import error with limbo.py
