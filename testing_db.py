2965a8effdfc3187871b5db5cc8f7dc875e52502
andrefei <andreas.eikeland@hotmail.com>
2017-03-30 12:10:47 +0200

    New way to catch systemoutput in testing_fb

--- testing_db.py
+++ testing_db.py
@@ -5,17 +5,27 @@ from io import StringIO
 import user_methods
 
 
+class Capturing(list):
+    def __enter__(self):
+        self._stdout = sys.stdout
+        sys.stdout = self._stringio = StringIO()
+        return self
+
+    def __exit__(self, *args):
+        self.extend(self._stringio.getvalue().splitlines())
+        del self._stringio    # free up some memory
+        sys.stdout = self._stdout
+
+
 class DbTests(unittest.TestCase):
 
     def test_user_methods(self):
         user = "Test User1337"
         subject = "TDT4120"
         user_methods.add_user(user, subject)
-        user_methods.add_user(user, subject)
-        captured_output = StringIO()
-        sys.stdout = captured_output
-        sys.stdout = sys.__stdout__
-        self.assertEqual('User already exists', captured_output.getvalue())
+        with Capturing as output:
+            user_methods.add_user(user, subject)
+        self.assertEqual('User already exists', str(output[0]))
         self.assertEqual(user_methods.get_subject_from_user("Test User1337"), "TDT4120")
         user_methods.add_subject("Test User1337", "TMA4100")
         self.assertEqual(user_methods.get_subject_from_user("Test User1337"), "TMA4100")
