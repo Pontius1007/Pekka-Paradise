9a25d3af33c08f731b58b082c0a8046a057c5d73
Ola Richard Syversen <olasy95@gmail.com>
2017-04-03 17:08:11 +0200

    testing combined

---
+++ testmaster.py
@@ -0,0 +1,10 @@
+__author__ = 'OlaRichard'
+# -*- coding utf-8 -*-
+import testing
+import testing_db
+
+test1 = testing.Testerino()
+test2 = testing_db.DbTests()
+if __name__ == '__main__':
+    test1
+    test2
