fbcd561e6672277dd9af7b218730153db9870d44
andrefei <andreas.eikeland@hotmail.com>
2017-04-04 12:29:10 +0200

    Split test methods into different classes and structured code with teardown and setUp methods

--- testing_methods.py
+++ testing_methods.py
@@ -1,6 +1,9 @@
 # -*- coding utf-8 -*-
 import sys
 from io import StringIO
+
+from sqlalchemy.exc import SQLAlchemyError
+
 import user_methods
 import random
 import feedback_methods
@@ -15,7 +18,7 @@ import subject_info
 
 class Capturing(list):
     """
-    This class is only for helping 'catch' info written to console
+    This class is for helping 'catch' or read text written to console
     """
     def __enter__(self):
         self._stdout = sys.stdout
@@ -243,49 +246,57 @@ class IMETest(unittest.TestCase):
         self.assertEqual(message_split.message_split("heidu"), ["heidu"])
 
 
-class DbTests(unittest.TestCase):
+class UserMethodTests(unittest.TestCase):
+
+    def setUp(self):
+        """
+        Fills the database with test user and test subject
+        :return: 
+        """
+        self.user = "Test User1337"
+        self.subject = "TDT4120"
+        self.test_sub = "TST4" + str(random.randint(0, 2000))
+        # Checks that added user is added to database with correct subject
+        user_methods.add_user(self.user, self.subject)
 
     def test_user_methods(self):
         """
         Tests the various methods in user_methods.py as described onwards
         :return: 
         """
-        user = "Test User1337"
-        subject = "TDT4120"
-        test_sub = "TST4" + str(random.randint(0, 2000))
-        # Checks that added user is added to database with correct subject
-        user_methods.add_user(user, subject)
         with Capturing() as output:
             # Checks that there is some response if user is already in db
-            user_methods.add_user(user, subject)
+            user_methods.add_user(self.user, self.subject)
         if len(output) > 0:  # This should always trigger as long as the DB is initially empty
             self.assertEqual('User already exists', str(output[0]))
-        self.assertTrue(user_methods.has_user(user))
-        self.assertEqual(user_methods.get_subject_from_user(user), subject)
+        self.assertTrue(user_methods.has_user(self.user))
+        self.assertEqual(user_methods.get_subject_from_user(self.user), self.subject)
 
         # Updates subject for user and checks that it updates in db
-        user_methods.add_subject(user, test_sub)
-        self.assertEqual(user_methods.get_subject_from_user(user), test_sub)
+        user_methods.add_subject(self.user, self.test_sub)
+        self.assertEqual(user_methods.get_subject_from_user(self.user), self.test_sub)
         # Removes user then subject and verifies that they are removed from db
-        user_methods.delete_user(user)
-        self.assertFalse(user_methods.has_user(user))
-        user_methods.remove_subject(test_sub)
-        self.assertFalse(user_methods.subject_has_subject(test_sub))
+        user_methods.delete_user(self.user)
+        self.assertFalse(user_methods.has_user(self.user))
+        user_methods.remove_subject(self.test_sub)
+        self.assertFalse(user_methods.subject_has_subject(self.test_sub))
         # Adds and removes subject independently from user
-        user_methods.add_subject_to_subject_table(test_sub)
-        self.assertTrue(user_methods.subject_has_subject(test_sub))
-        user_methods.remove_subject(test_sub)
-        self.assertFalse(user_methods.subject_has_subject(test_sub))
+        user_methods.add_subject_to_subject_table(self.test_sub)
+        self.assertTrue(user_methods.subject_has_subject(self.test_sub))
+        user_methods.remove_subject(self.test_sub)
+        self.assertFalse(user_methods.subject_has_subject(self.test_sub))
 
         # Now assume methods for adding and removing subjects and users are working
         # Add new user with subject not in db, just to test
-        self.assertFalse(user_methods.subject_has_subject(test_sub))
-        user_methods.add_user(user, test_sub)
-        self.assertTrue(user_methods.subject_has_subject(test_sub))
-        user_methods.delete_user(user)
-        user_methods.remove_subject(test_sub)
+        self.assertFalse(user_methods.subject_has_subject(self.test_sub))
+        user_methods.add_user(self.user, self.test_sub)
+        self.assertTrue(user_methods.subject_has_subject(self.test_sub))
 
-        # Check that user_methods throws exceptions when given wrong args
+    def test_exceptions(self):
+        """
+        Checks that the various methods throws exceptions when given wrong args
+        :return: 
+        """
         self.assertRaises(Exception, user_methods.has_user(1337))
         self.assertRaises(Exception, user_methods.subject_has_subject(1337))
         self.assertRaises(Exception, user_methods.add_subject_to_subject_table(1337))
@@ -293,42 +304,74 @@ class DbTests(unittest.TestCase):
         self.assertRaises(Exception, user_methods.add_user(1337, 1337))
         self.assertRaises(Exception, user_methods.add_subject(1337, 1337))
 
-    def test_feedback_methods(self):
+    def tearDown(self):
+        try:
+            user_methods.delete_user(self.user)
+        except SQLAlchemyError:
+            print("No user to remove in db")
+        try:
+            user_methods.remove_subject(self.test_sub)
+        except SQLAlchemyError:
+            print("No subject to remove db")
+
+
+class FeedbackMethodsTest(unittest.TestCase):
+
+    def setUp(self):
         """
-        This methods tests feedback methods, and assumes user_methods work.
-        Starts by adding 'dummy' user and lectures to db and then tests the 
-        various, methods in feedback_methods.py.        # subj year(int) week(int) day(int)  start_t end_t  room
+        Populates the database with various test data
         :return: 
         """
-        today = self.get_today()
-        name = "TESTER LASTNAME"
-        lecture_info = ["TST4200"]
-        for item in today:
-            lecture_info.append(item)
-        lecture_info.append("01:00")
-        lecture_info.append("24:59")
-        lecture_info.append("P")
-        # Add user and test lecture
-        user_methods.add_user(name, lecture_info[0])
-        lecture_methods.add_and_remove_test(False, lecture_info)
+        self.today = self.get_today()
+        self.name = "TESTER LAST_NAME"
+        self.lecture_info = ["TST4200"]
+        for item in self.today:
+            self.lecture_info.append(item)
+        self.lecture_info.append("01:00")
+        self.lecture_info.append("24:59")
+        self.lecture_info.append("P")
+        user_methods.add_user(self.name, self.lecture_info[0])
+        lecture_methods.add_and_remove_test(False, self.lecture_info)
 
+    def test_feedback_methods(self):
+        """
+        This methods tests feedback methods, and assumes user_methods work.      
+        lecture info : [subject(str), year(int), week(int), day(int), start_time(str), end_time(str), room(str)]
+        :return: 
+        """
         # Checks the various feedback methods
-        self.assertTrue(feedback_methods.add_entry(name, lecture_info[0], "1"))
-        self.assertFalse(feedback_methods.add_entry(name, lecture_info[0], "0"))
-        feedback_methods.remove_all_feedback(name)
-        # Add method to add feedback evaluation stuff
-        self.assertTrue(feedback_methods.add_entry(name, lecture_info[0], "1"))
-        self.assertEqual(feedback_methods.get_single_lecture_feed(lecture_info[1], lecture_info[2], lecture_info[3],
-                                                                  lecture_info[0])[1], [1])
-        feedback, feedback_evaluation = feedback_methods.get_all_subject_feed(lecture_info[0])
+        self.assertTrue(feedback_methods.add_entry(self.name, self.lecture_info[0], "1"))
+        self.assertFalse(feedback_methods.add_entry(self.name, self.lecture_info[0], "0"))
+        feedback_methods.remove_all_feedback(self.name)
+        # TODO add test for feedback evaluation
+        self.assertTrue(feedback_methods.add_entry(self.name, self.lecture_info[0], "1"))
+        self.assertEqual(feedback_methods.get_single_lecture_feed(self.lecture_info[1], self.lecture_info[2],
+                                                                  self.lecture_info[3], self.lecture_info[0])[1], [1])
+        feedback, feedback_evaluation = feedback_methods.get_all_subject_feed(self.lecture_info[0])
         self.assertEqual(feedback, [1])
 
-        # Remove user, test feedback and lecture
-        # TODO remove lecturefeedbackevaluation first
-        feedback_methods.remove_all_feedback(name)
-        lecture_methods.add_and_remove_test(True, lecture_info)
-        user_methods.delete_user(name)
-        user_methods.remove_subject(lecture_info[0])
+    def tearDown(self):
+        """
+        Removes test data from the database if it exists
+        :return: 
+        """
+        # TODO remove lecture_feedback_evaluation first
+        try:
+            feedback_methods.remove_all_feedback(self.name)
+        except SQLAlchemyError:
+            print("No feedback in db")
+        try:
+            lecture_methods.add_and_remove_test(True, self.lecture_info)
+        except SQLAlchemyError:
+            print("No test lecture in db")
+        try:
+            user_methods.delete_user(self.name)
+        except SQLAlchemyError:
+            print("No user in db")
+        try:
+            user_methods.remove_subject(self.lecture_info[0])
+        except SQLAlchemyError:
+            print("No subject in db")
 
     @staticmethod
     def get_today():
