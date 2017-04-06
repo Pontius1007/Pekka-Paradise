c5d1ed4cc2de82cf8e6f0af8602eb49fe6cb3a46
Håvard Hellem <haavardhellem@gmail.com>
2017-04-06 13:33:27 +0200

    migration versions 25 and 26

--- db_repository/versions/025_migration.py
+++ db_repository/versions/025_migration.py
@@ -5,17 +5,17 @@ from migrate import *
 from migrate.changeset import schema
 pre_meta = MetaData()
 post_meta = MetaData()
-lecturefeedbakevaluation = Table('lecturefeedbakevaluation', post_meta,
+lecturefeedbackevaluation = Table('lecturefeedbackevaluation', post_meta,
     Column('id', Integer, primary_key=True, nullable=False),
     Column('user_id', String(length=100)),
     Column('lecture_id', Integer),
     Column('increased_knowledge', Integer),
     Column('well_organized', Integer),
-    Column('logical', Integer),
     Column('use_of_slides', Integer),
     Column('use_of_time', Integer),
     Column('presenter_knowledgeable', Integer),
     Column('general_score', Integer),
+    Column('next_lecture', Integer),
 )
 
 
@@ -24,11 +24,11 @@ def upgrade(migrate_engine):
     # migrate_engine to your metadata
     pre_meta.bind = migrate_engine
     post_meta.bind = migrate_engine
-    post_meta.tables['lecturefeedbakevaluation'].create()
+    post_meta.tables['lecturefeedbackevaluation'].create()
 
 
 def downgrade(migrate_engine):
     # Operations to reverse the above upgrade go here.
     pre_meta.bind = migrate_engine
     post_meta.bind = migrate_engine
-    post_meta.tables['lecturefeedbakevaluation'].drop()
+    post_meta.tables['lecturefeedbackevaluation'].drop()
